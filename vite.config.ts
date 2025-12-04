28ec9f174e6d76ffb9979130d21636c202beb77f
Julio Caicedo <julio.caicedo@startcodex.com>
2025-12-04 02:09:08 -0500

    feat: Initialize a new Keycloak theme project with Storybook, Vite, and CI/CD setup.

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { keycloakify } from "keycloakify/vite-plugin";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [
+        react(),
+        tailwindcss(),
+        keycloakify({
+            accountThemeImplementation: "none"
+        })
+    ]
+});
